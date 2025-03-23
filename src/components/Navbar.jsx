b096a98f528d0749ca5e4b47a45d231bf84bd09e
Deepika P <128278861+deepz0118@users.noreply.github.com>
2025-03-24 00:11:26 +0530

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,8 @@ function Navbar() {
       <ul className="text-2xl text-teal-500 flex space-x-4">
         <li><Link to="/" className="hover:text-gray-200 transition-colors">Home</Link></li>
         <li><Link to="/login" className="hover:text-gray-200 transition-colors">Login</Link></li>
-        <li><Link to="/user" className="hover:text-gray-200 transition-colors">User</Link></li>
+        {/* <li><Link to="/user" className="hover:text-gray-200 transition-colors">User</Link></li> */}
+        <li><Link to="/programs" className="hover:text-gray-200 transition-colors">Programs</Link></li>
         {/* <li><Link to="/dashboard" className="hover:text-gray-200 transition-colors">Dashboard</Link></li>
         <li><Link to="/claimfood" className="hover:text-gray-200 transition-colors">Claim</Link></li> */}
       </ul>
